18a3d8374442dceee9d981e5d45552911a791b25
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-05-10 22:37:42 +0300

    new

---
+++ client/src/LabTech/LabTechnician.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+import './LabTechnician.css';
+import axios from 'axios';
+
+
+function LabTechnician(){
+
+    const [requests , setRequest] = useState([])
+    
+    useEffect(() => {
+
+        const fetchUncheckedRequest = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/api/labrequest')
+                setRequest(response.data);
+            } catch (error) {
+                console.error('Failed to fetch unchecked elements:', error);
+            }
+        };
+        fetchUncheckedRequest();
+
+    })
+    
+    async function deleteRequest(id){
+        try {
+            await axios.put(`http://localhost:3001/update/${id}`, { checked: true });
+            //setRequest(requests.filter(item => item._id !== id));
+        } catch (error) {
+            console.error('Failed to update element:', error);
+        }
+    
+        console.log(id)
+    }
+    const buttonStyle = {
+        
+        color: 'red',
+        backgroundColor: 'rgb(233, 241, 241)',
+        padding: '10px',
+        border: "none",
+        cursor: "pointer",
+        paddingTop: '1',
+        paddingBottom: '1',
+      };
+
+    const liStyle = {
+        //paddingLeft: '20px',
+        listStyleType: 'none',
+    }
+    return(
+        <div className="main-element">
+            <div className="labpage">
+                <div className="announcement">
+                    <h1 className='title1'>Announcement</h1>
+                    <div className="element">
+
+                    </div>
+                </div>
+                <div className="todo-list">
+                   <h1 className='title2'>To do list</h1>
+                   <div className="list">
+
+
+                        <ol >
+                            {
+                                requests.map((request, index ) =>{
+                                    return <li key={request._id} style={{padding:'2px' , paddingLeft: '20px',margin: "0px", listStyleType: 'none' }}>
+                                        <span>  {index + 1}. &nbsp; Dr. {request.doctorName}&nbsp; &nbsp;
+                                                patient name: {request.patientName} &nbsp; &nbsp;
+                                                patientID: {request.patientID} &nbsp; &nbsp;
+                                                Type: {request.labTestType}&nbsp; &nbsp;
+                                                {request.urgency && <span>urgency</span> }&nbsp;
+                                        </span>
+                                        
+                                        <button onClick={() => deleteRequest(request._id)} style={buttonStyle}>delete</button>
+                                    </li>
+                                })
+                            }
+                        </ol>
+
+                        
+
+
+                        
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LabTechnician
--- server/index.js
+++ server/index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const labProcessRoutes = require('./LabReq/labProcessRoutes');
-const logins= require('./loging/logins');
-const appkida= require('./Backend_Appointment/appkida');
-const feed = require('./Feedback/feed')
-const human_res =require('./The_Manager/human-res')
 const Router = require('./router/Router')
 const app = express();
-
-const { patientDashboard, doctorDashboard } = require('./controllers/dashboardController');
-const { loginController, signupController, checkSession,
-    sendPasswordResetMail, verifyOtp, changePassword } = require('./controllers/authController');
 const cors = require('cors');
 require('dotenv').config();
 
-
-require('./db');
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://FirstMongo:mongo123@hospitalmanagementsyste.mq1fdvh.mongodb.net/Hospital_Management_System', {
   // useNewUrlParser: true,
@@ -28,24 +17,54 @@ mongoose.connect('mongodb+srv://FirstMongo:mongo123@hospitalmanagementsyste.mq1f
 // Middleware
 app.use(cors()); // Add this line to enable CORS
 app.use(bodyParser.json()); 
+//lab request display
+app.put('/update/:id', async (req, res) => {
+  try {
+      const id = req.params.id; // Get the ID of the request to update
+      // Update the request with the specified ID to set 'checked' to true
+      await requestModel.findByIdAndUpdate(id, { checked: true });
+      // Respond with a success message
+      res.status(200).json({ message: 'Document updated successfully' });
+  } catch (error) {
+      // If an error occurs, respond with an error message
+      res.status(500).json({ error: 'Failed to update document' });
+  }
+});
 
-// Routes
-app.use('/', labProcessRoutes);
-app.use('/api', logins);
-app.use('/',feed)
-app.use('/api', appkida);
-app.use('/', human_res);
-app.use('/', Router);
-//forlogin
-app.post('/api/login', loginController); 
-app.post('/api/signup', signupController);
-app.post('/api/forgotPassword', sendPasswordResetMail);
-app.post('/api/verifyOtp', verifyOtp);
-app.post('/api/changePassword', changePassword);
+// Define the GET endpoint for retrieving unchecked lab requests
+app.get('/api/labrequest', (req, res) => {
+  // Find all lab requests where 'checked' is false
+  requestModel.find({ checked: false })
+      .then(requests => res.json(requests)) // Respond with the found requests
+      .catch(err => res.json(err)); // If an error occurs, respond with the error
+});
+//lab report send
+app.post('/api/labresult', async (req, res) => {
+  try {
+      const newTest = req.body;
 
-app.get('/api/checkSession', checkSession);
-app.get('/api/dashboard/patient', patientDashboard);
-app.get('/api/dashboard/doctor', doctorDashboard);
+      // Check if required fields are present
+      if (!newTest.patientID || !newTest.requestID || !newTest.person || !newTest.ID) {
+          console.log("Please fill all required field");
+          // Respond with a 400 status code and an error message if any required field is missing
+          return res.status(400).json({ error: 'Please fill all required field' });
+      }
+
+      // Create a new document based on the request body and save it to the database
+      const newForm = new reportModel(newTest);
+      const response = await newForm.save();
+      console.log(response);
+      
+      // Respond with a 201 status code and a success message
+      res.status(201).json({ message: 'Laboratory report successfully sent!' });
+  } catch (error) {
+      // If an error occurs, respond with a 500 status code and an error message
+      console.error('Failed to submit form:', error);
+      res.status(500).json({ error: 'Unable to report data. Try again later.' });
+  }
+});
+
+
 
 
 // Start the server
